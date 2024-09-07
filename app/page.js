e4b6e6f7478056d0bbc7588f2fc734040022ffb1
adarshraj2720 <adarshraj2721@gmail.com>
2024-09-07 17:37:42 +0530

    solve build error

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 export default  function Home() {
   const router = useRouter();
   const Computerdata = [
@@ -43,6 +44,7 @@ export default  function Home() {
       img: "https://quizard.app/images/ScienceQuiz.png",
     },
   ];
+useEffect(()=>{
 
   window.addEventListener('scroll', () => {
     const navbar = document.getElementById('navbar');
@@ -53,6 +55,7 @@ export default  function Home() {
       navbar?.classList?.remove('scrolled');
     }
   });
+},[])
 
   return (
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
